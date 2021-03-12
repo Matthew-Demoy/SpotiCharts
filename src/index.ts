22ecbe8c886f56f3566d5f8c30e9d8aa0e6b2d63
Matthew Demoy <MatthewDemoy@student.uml.edu>
2021-03-11 23:22:18 -0500

    access control allow origins

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,11 @@ createConnection({ ...config, entities } as any)
     // create express app
     
     const app = express();
-    
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
     app.use(bodyParser.json());
 
     // register all application routes
